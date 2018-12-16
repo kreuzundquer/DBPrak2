dce7484f923e056ad8bebe341a2f3a9ea447bc5e
pkhairkh <parham.khairkhah@smail.th-koeln.de>
2018-12-16 14:26:34 +0100

    Bpd.java

--- Bpd.java
+++ Bpd.java
@@ -1,6 +1,26 @@
+import java.sql.*;
 
 public class Bpd {
+	
+	private Connection connection;
+
 	int posnr, artnr, anzbo, menge, algrad;
 	String kuehl, artbez;
-	boolean verpackt;
+	boolean connection;
+
+	static String stmtBST = "SELECT * FROM BESTELLUNG WHERE STATUS=";
+
+	public Bpd(Connection connection) {
+		this.connection = connection;
+	}
+
+	static public LinkedLiest<String> show(int status){
+		Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(stmtWILDCARD);
+
+        LinkedList<String> results = new LinkedList<>();
+        String Infostring;
+
+        while (rs.next()){
+	}
 }
