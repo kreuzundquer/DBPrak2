a2fd13e2ec25da2ccaeda4adf78848debb587947
pkhairkh <parham.khairkhah@smail.th-koeln.de>
2018-12-16 14:38:12 +0100

    Bpd.java

--- Bpd.java
+++ Bpd.java
@@ -8,7 +8,7 @@ public class Bpd {
 	String kuehl, artbez;
 	boolean connection;
 
-	static String stmtBST = "SELECT * FROM BESTELLUNG WHERE STATUS=";
+	static String stmt = "SELECT * FROM BESTELLUNG WHERE STATUS= ";
 
 	public Bpd(Connection connection) {
 		this.connection = connection;
@@ -16,11 +16,20 @@ public class Bpd {
 
 	static public LinkedLiest<String> show(int status){
 		Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(stmtWILDCARD);
+        ResultSet rs = stmt.executeQuery(stmt + status);
 
         LinkedList<String> results = new LinkedList<>();
         String Infostring;
 
         while (rs.next()){
+			Infostring = "BSTNR :" + rs.getInt("BSTNR") +
+            			"\nKNR :" + rs.getString("KNR") +
+            			"\nSTATUS :" + rs.getString("STATUS") +
+                        "\nRSUM :" + rs.getDouble("RSUM");
+						
+			results.add(Infostring);
+		}
+
+		return results;
 	}
 }
